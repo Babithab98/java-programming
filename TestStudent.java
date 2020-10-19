05bfc76b0f8a5003f91c673e79dfce41447e6e91
Babithab98 <72904469+Babithab98@users.noreply.github.com>
2020-10-19 14:23:03 +0530

    Add files via upload

---
+++ TestStudent.java
@@ -0,0 +1,24 @@
+class TestStudent {
+	public static void main(String[] args){
+	Student S1 = new Student();
+	S1.id = 1;
+	S1.name = "babitha";
+System.out.println(S1.id+" "+S1.name);
+	Student S2 = new Student();
+	S2.id = 2;
+	S2.name = "Anusha";
+System.out.println(S2.id+" "+S2.name);
+	Student S3 = new Student();
+	S3.id = 3;
+	S3.name = "Arpitha";
+System.out.println(S3.id+" "+S3.name);
+	Student S4 = new Student();
+	S4.id = 4;
+	S4.name = "Naheeda";
+System.out.println(S4.id+" "+S4.name);
+	Student S5 = new Student();
+	S5.id = 5;
+	S5.name = "Chandrakanth";
+System.out.println(S5.id+" "+S5.name);
+}
+}
